24a2dfd8bf78da173400edd4dbc120479742adc1
hermitAT <ahthornev2@gmail.com>
2020-12-10 13:22:28 -0800

    Add useVisualMode fn to transition/back between components of app

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = function(newMode, replace = false) {
+    if (replace) {
+      history.pop();
+      history.push(newMode);
+      setMode(history[history.length - 1]);
+    } else {
+      history.push(newMode);
+      setMode(history[history.length - 1]);
+    }3
+  };
+
+  const back = function() {
+    if (history.length === 1) {
+      return;
+    }
+
+    history.pop();
+    setMode(history[history.length - 1]);
+  };
+
+  return { mode, transition, back };
+};
